b6fac7cb11fd6cb3ffe10a7e323e5ef79860efda
mpierre24 <pierremlox@gmail.com>
2021-08-28 22:41:12 -0400

    Adds define columns for product

--- models/Product.js
+++ models/Product.js
@@ -2,14 +2,29 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    product_name: { type: DataTypes.STRING, allowNull: false },
+    price: { type: DataTypes.DECIMAL(10, 2) },
+    stock: { type: DataTypes.INTEGER, defaultValue: 0 },
+    category_id: {
+      type: DataTypes.INTEGER,
+
+      references: {
+        // This is a reference to another model
+        model: Category,
+
+        // This is the column name of the referenced model
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
