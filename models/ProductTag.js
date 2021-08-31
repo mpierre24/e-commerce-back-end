c683563f57709d1e26cfc4133fc9fc2fd91909b0
mpierre24 <pierremlox@gmail.com>
2021-08-31 19:52:04 -0400

    Adds product tag through table and relationships

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,36 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
+const Tag = require('./Tag');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
-    // define columns
+    // define columns 
+
+    product_id: {
+      type: DataTypes.INTEGER,
+
+      references: {
+        // This is a reference to another Product
+        model: Product,
+
+        // This is the column name of the referenced model
+        key: 'id'
+      }
+    }, tag_id: {
+      type: DataTypes.INTEGER,
+
+      references: {
+        // This is a reference to another model
+        model: Tag,
+
+        // This is the column name of the referenced model
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
